da8bd2d03ef5447c9d5008b56756f36bb6605407
Benjamin Maurin <benjamin.maurin@axilumrobotics.com>
2023-08-25 13:43:53 +0200

    Update SDK files

--- SDK/inc/gen-versioninfo.h
+++ SDK/inc/gen-versioninfo.h
@@ -5,9 +5,9 @@
 #define VER_M_ 1
 #define VER_L_ 3
 // Internal use only
-#define BUILD 40
+#define BUILD 43
 
 #define FILEVER				"2, 2, 1, 3"
 #define PRODVER_TAG			"v2.2.1.3"
-#define PRODVER_SHA1		"60cf2f"
+#define PRODVER_SHA1		"c113f5"
 
