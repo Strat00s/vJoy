04cc21e0c96d657fbbd955ab91869b176ffc2cfd
Benjamin Maurin <benjamin.maurin@axilumrobotics.com>
2023-08-29 11:50:24 +0200

    Cleanup external dlls

--- SDK/inc/gen-versioninfo.h
+++ SDK/inc/gen-versioninfo.h
@@ -5,9 +5,9 @@
 #define VER_M_ 1
 #define VER_L_ 3
 // Internal use only
-#define BUILD 46
+#define BUILD 47
 
 #define FILEVER				"2, 2, 1, 3"
 #define PRODVER_TAG			"v2.2.1.3"
-#define PRODVER_SHA1		"7a3738"
+#define PRODVER_SHA1		"14a643"
 
